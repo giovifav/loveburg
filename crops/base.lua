7c82f7f802383438775279063a990914fb0bd026
giovifav <wip.gio@gmail.com>
2022-08-20 11:27:01 +0200

    save1

--- crops/base.lua
+++ crops/base.lua
@@ -1,20 +1,20 @@
 local crop = Class:extend()
 
-function crop:new(x,y)
+function crop:new(x, y)
     self.name = "generic crop"
     self.growTime = 1 -- in seconds
     self.paused = false
 
-    self.sprite = love.graphics.newImage("assets/crops/ground/eggplant/eggplant_all_frames.png")
-    local w,h = self.sprite:getDimensions()
-    self.quads = {}
-    self.quads.semi = love.graphics.newQuad(0,0,16,32,w,h)
-    self.quads.germoglio = love.graphics.newQuad(16,0,16,32,w,h)
-    self.quads.acerbo = love.graphics.newQuad(32,0,16,32,w,h)
-    self.quads.inMaturazione = love.graphics.newQuad(48,0,16,32,w,h)
-    self.quads.pronto  = love.graphics.newQuad(64,0,16,32,w,h)
-    self.quads.troppoMaturo = love.graphics.newQuad(80,0,16,32,w,h)
-    self.x, self.y = x, y
+    self.sprite              = love.graphics.newImage("assets/crops/ground/carrot/carrot_all_frames.png")
+    local w, h               = self.sprite:getDimensions()
+    self.quads               = {}
+    self.quads.semi          = love.graphics.newQuad(0, 0, 16, 32, w, h)
+    self.quads.germoglio     = love.graphics.newQuad(16, 0, 16, 32, w, h)
+    self.quads.acerbo        = love.graphics.newQuad(32, 0, 16, 32, w, h)
+    self.quads.inMaturazione = love.graphics.newQuad(48, 0, 16, 32, w, h)
+    self.quads.pronto        = love.graphics.newQuad(64, 0, 16, 32, w, h)
+    self.quads.troppoMaturo  = love.graphics.newQuad(80, 0, 16, 32, w, h)
+    self.x, self.y           = x, y
 
     local states = {
         default = {
@@ -23,7 +23,7 @@ function crop:new(x,y)
             { quad = self.quads.acerbo, duration = self.growTime },
             { quad = self.quads.inMaturazione, duration = self.growTime },
             { quad = self.quads.pronto, duration = self.growTime },
-            { quad = self.quads.troppoMaturo, duration = self.growTime  }
+            { quad = self.quads.troppoMaturo, duration = self.growTime }
         }
     }
     self.behavior = Behavior(states)
@@ -31,19 +31,20 @@ function crop:new(x,y)
 
 
 end
+
 function crop:pause()
     self.paused = not self.paused
 end
 
 function crop:update(dt)
     if not self.paused then
-    self.behavior:update(dt)
+        self.behavior:update(dt)
     end
 end
 
 function crop:draw()
-    
-    love.graphics.draw(self.sprite, self.behavior.frame.quad, self.x,self.y)
+
+    love.graphics.draw(self.sprite, self.behavior.frame.quad, self.x, self.y)
 end
 
 return crop
